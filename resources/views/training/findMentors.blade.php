5af44060cfac374795ee75f10797d6f7cc393e47
richmondrroldan <richmondrroldan@gmail.com>
2017-10-20 09:36:30 +0800

    Searchw

--- resources/views/training/findMentors.blade.php
+++ resources/views/training/findMentors.blade.php
@@ -11,9 +11,26 @@
                     <hr class="intro-divider">
                     <ul class="list-inline intro-social-buttons">
                         <li>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        
+
+                                        {!! Form::model($skills, ['method' => 'GET','route' => ['results.index']]) !!}
+                                        {!! Form::text('skill', null, array('placeholder' => 'Search','class' => 'form-control')) !!}
+                                        <button type="submit" class="btn btn-default btn-lg span"><span class="network-name">SEARCH</span></button>
+                                        {!! Form::close()!!}
+
+                                     </div>
+                                </div>
+                            </div>
+
+
                         @foreach($skills as $skill)
                             <a href="mSpec/{{$skill->title}}" class="btn btn-default btn-lg span"> <span class="network-name">{{$skill->title}}</span></a><br><br>
                         @endforeach
+
+
                         </li>
                         </br>
                         </br>
