373b74a39bbeb1efa6484fa4cdf3be86f07e26b5
richmondrroldan <richmondrroldan@gmail.com>
2018-01-03 15:05:13 +0800

    Other fix

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -48,37 +48,3 @@
     
 @include('templates/footer')
 @stop
-
-<<<<<<< HEAD
-=======
-=======
-
-<div class="intro-header">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="intro-message">
-                    <h3>RESULTS</h3>
-                    <hr class="intro-divider">
-                    <ul class="list-inline intro-social-buttons">
-                        <li>
-                            @foreach($results as $result)
-                                <a href="/mDet/{{$result->id}}" > <span class="network-name">{{$result->name}}</span></a><br><br>
-                            @endforeach
-                        </li>
-                        </br>
-                        </br>
-                        </br>
-                        <li>
-                        <a href="/findMentors" class="btn btn-default btn-lg span"> <span class="network-name">Back</span></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-</div
-@include('templates/footer')
-@stop
->>>>>>> 1c73d7bc9c562d05d74ed7caf991ed22bb82721e
->>>>>>> login
