3b7691ec6b8ff2b8206d3f1d8f783b94e25a402c
richmondrroldan <richmondrroldan@gmail.com>
2018-02-19 15:48:03 +0800

    fuck

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,25 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-	<title>Appollo</title>
+    <title>Appollo</title>
 
-	<!-- Bootstrap Core CSS -->
+    
+    <!-- Bootstrap Core CSS -->
     <link href="/css/bootstrap.css" rel="stylesheet">
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/sb-admin-2.css" rel="stylesheet">
     
-    <!-- Custom CSS -->
+    <!-- Custom CSS --> 
     <link href="/css/landing-page.css" rel="stylesheet">
+    
 
     <!-- Custom Fonts -->
     <link href="/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito|Poppins" rel="stylesheet">
+    
     @yield('nav')
 </head>
 <body>
@@ -32,5 +35,5 @@
     @yield('jsFile')
 @yield('footer')
 </body>
-	
+    
 </html>
