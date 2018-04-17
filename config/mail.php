80e0d67eb37f812c60f37af41127da108eeebbfb
richmondrroldan <richmondrroldan@gmail.com>
2018-04-17 17:45:29 +0800

    email

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'richmondrroldan@gmail.com', 'name' => 'rich'],
 
     /*
     |--------------------------------------------------------------------------
